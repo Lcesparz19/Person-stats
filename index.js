5d934455f0b32176a12ddc6dd45420075c543fee
Lcesparz19 <lucacruztd@gmail.com>
2017-06-26 23:02:09 -0400

    Add the name to a paragraph inside the stats div. Add another input to the form. Display the value of that input alongside the name in the div (or p). Change the appearance of the paragraph (think CSS) based on a value from the form. e.g. Turn the text blue if the user types blue in the form field.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ function changeHeading(ev) {
     const f = ev.target
     const name = f.personName.value
     const age = f.personAge.value
+    const color = f.color.value
     const heading = document.querySelector('h1')
     const divname = document.querySelector('#divname')
     const divage = document.querySelector('#divage')
     heading.textContent = name + "'s Stats"
     divname.textContent = "Name: " + name
     divage.textContent = "Age: " + age
+    document.getElementById("stats").style.color = color
 }
   const personForm = document.querySelector('#person-form')
   personForm.addEventListener('submit',changeHeading)
