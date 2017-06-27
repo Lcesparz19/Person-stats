5cc63cd6fc0316b2936c1fc0647d509c582a97ce
Lcesparz19 <lucacruztd@gmail.com>
2017-06-26 22:27:49 -0400

    Update the new stats div with the value of the text input.

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ function changeHeading(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
+    const age = f.personAge.value
     const heading = document.querySelector('h1')
-    heading.textContent = name
+    const divname = document.querySelector('#divname')
+    const divage = document.querySelector('#divage')
+    heading.textContent = name + "'s Stats"
+    divname.textContent = "Name: " + name
+    divage.textContent = "Age: " + age
 }
   const personForm = document.querySelector('#person-form')
   personForm.addEventListener('submit',changeHeading)
