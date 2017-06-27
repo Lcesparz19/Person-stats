6ed6f53013029f9be4a78039a2d39ddab9764975
Lcesparz19 <lucacruztd@gmail.com>
2017-06-27 18:43:21 -0400

    Added lists and favorite color

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 function handleSubmit(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const name = f.personName.value
-    const age = f.personAge.value
-    const color = f.color.value
-    const heading = document.querySelector('h1')
-    const divname = document.querySelector('#divname')
-    const divage = document.querySelector('#divage')
-    heading.textContent = name + "'s Stats"
-    divname.textContent = "Name: " + name
-    divage.textContent = "Age: " + age
-    document.getElementById("stats").style.color = color
+  ev.preventDefault()
+  const f = ev.target
+
+  const name = f.personName.value
+  const age = f.age.value
+  const favoriteColor = f.favoriteColor.value
+
+  const list = document.createElement('ul')
+
+  const nameItem = document.createElement('li')
+  nameItem.textContent = `Name: ${name}`
+  list.appendChild(nameItem)
+
+  const ageItem = document.createElement('li')
+  ageItem.textContent = `Age: ${age}`
+  list.appendChild(ageItem)
+
+  const colorItem = document.createElement('li')
+  colorItem.textContent = 'Favorite Color: '
+
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = favoriteColor
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+  colorItem.appendChild(colorDiv)
+
+  list.appendChild(colorItem)
+
+  const stats = document.querySelector('#stats')
+  stats.appendChild(list)
 }
-  const personForm = document.querySelector('#person-form')
-  personForm.addEventListener('submit',handleSubmit)
+
+const personForm = document.querySelector('#person-form')
+personForm.addEventListener('submit', handleSubmit)
