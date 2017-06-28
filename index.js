062ea1856761f359ee2ee9ea9a155f0b071be094
Lcesparz19 <lucacruztd@gmail.com>
2017-06-27 23:03:33 -0400

    created renderList function to to create stats list from renderListItem

--- index.js
+++ index.js
@@ -6,31 +6,7 @@ function handleSubmit(ev) {
   const age = f.age.value
   const favoriteColor = f.favoriteColor.value
 
-  const list = document.createElement('ul')
-
-  const nameItem = document.createElement('li')
-  nameItem.textContent = `Name: ${name}`
-  list.appendChild(nameItem)
-
-  const ageItem = document.createElement('li')
-  ageItem.textContent = `Age: ${age}`
-  list.appendChild(ageItem)
-
-  const colorItem = document.createElement('li')
-  colorItem.textContent = 'Favorite Color: '
-/*
-  const colorDiv = document.createElement('div')
-  colorDiv.style.backgroundColor = favoriteColor
-  colorDiv.style.width = '6rem'
-  colorDiv.style.height = '3rem'
-  */
-  const colorDiv = renderColor(favoriteColor)
-  colorItem.appendChild(colorDiv)
-
-  
-
-  list.appendChild(colorItem)
-
+  const list = renderList(name,age,favoriteColor)
   const stats = document.querySelector('#stats')
   stats.appendChild(list)
 }
@@ -41,5 +17,31 @@ function renderColor(favoriteColor){
   colorDiv.style.height = '3rem'
   return colorDiv
 }
+function renderListItem(itemName,value){
+    const listItem = document.createElement('li')
+  listItem.textContent = `${itemName}: ${value}` 
+return listItem 
+}
+function renderList(name,age,favoriteColor){
+ const list = document.createElement('ul')
+
+  const nameItem = renderListItem('name',name)
+
+  const ageItem = renderListItem('age',age)
+  
+  const colorItem = renderListItem('favoriteColor',favoriteColor)
+  list.appendChild(nameItem)
+  list.appendChild(ageItem)
+  
+
+  const colorDiv = renderColor(favoriteColor)
+  colorItem.appendChild(colorDiv)
+
+  
+
+  list.appendChild(colorItem)
+return list 
+}
+
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', handleSubmit)
